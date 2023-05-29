0c4e4f3f95348a6a7835e6a0411272d4076fd0db
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2023-05-29 20:42:48 +0200

    basic changes

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ import { Button } from "@mui/material";
 
 import { useNavigate } from "react-router-dom";
 
-export const NotFount = () => {
+export const NotFound = () => {
   const n = useNavigate();
 
   return (
