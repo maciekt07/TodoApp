25665f10f4cc1471a916b1833693db71f6649533
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2023-08-29 01:24:01 +0200

    basic changes

---
+++ src/router.tsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import { UserProps } from "./types/user";
+import { UserSettings } from "./pages/UserSettings";
+import { Categories } from "./pages/Categories";
+import { NotFound } from "./pages/NotFound";
+import { AddTask } from "./pages/AddTask";
+import { ImportExport } from "./pages/ImportExport";
+import { Home } from "./pages/Home";
+
+export const AppRouter = ({ user, setUser }: UserProps): JSX.Element => {
+  const userProps = { user, setUser };
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home {...userProps} />} />
+      <Route path="/add" element={<AddTask {...userProps} />} />
+      <Route path="/user" element={<UserSettings {...userProps} />} />
+      <Route path="/import-export" element={<ImportExport {...userProps} />} />
+      <Route path="/categories" element={<Categories {...userProps} />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
