04abe12fa21f75e26764ba69fa53e99a88f01317
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2024-03-10 23:04:20 +0100

    add feature to download qr code

---
+++ src/utils/saveQRCode.ts
@@ -0,0 +1,61 @@
+import type { Task, UUID } from "../types/user";
+/**
+ *  Function to handle downloading QR code as PNG image
+ */
+export const saveQRCode = (tasks: Task[], selectedTaskId: UUID | null) => {
+  const svgElement = document.getElementById("QRCode") as SVGElement | null;
+  if (!svgElement) {
+    console.error("QR Code not found.");
+    return;
+  }
+
+  const svgData = new XMLSerializer().serializeToString(svgElement);
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  if (!ctx) {
+    console.error("Canvas context not supported.");
+    return;
+  }
+
+  const img = new Image();
+  img.onload = () => {
+    const taskName = tasks.find((task) => task.id === selectedTaskId)?.name || "";
+    const canvasWidth = img.width + 20;
+    const canvasHeight = img.height + 70;
+
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    canvas.style.borderRadius = "10px";
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    // Draw QR code
+    const qrCodeX = (canvasWidth - img.width) / 2;
+    const qrCodeY = 10;
+    ctx.drawImage(img, qrCodeX, qrCodeY);
+    // Draw task name
+    ctx.font = "bold 20px Poppins";
+    const textWidth = ctx.measureText(taskName).width;
+    const centerX = (canvasWidth - textWidth) / 2;
+    const bottomY = canvasHeight - 35;
+
+    ctx.fillStyle = "black";
+    ctx.fillText(taskName, centerX, bottomY);
+    // Draw attribution text
+    const text2 = "Todo App by github.com/maciekt07";
+    ctx.font = "14px Poppins";
+    const text2Width = ctx.measureText(text2).width;
+    const text2X = (canvasWidth - text2Width) / 2;
+    const text2Y = canvasHeight - 10;
+    ctx.fillText(text2, text2X, text2Y);
+    // Convert canvas to PNG data URL
+    const pngFile = canvas.toDataURL("image/png");
+    // Create download link
+    const downloadLink = document.createElement("a");
+    downloadLink.download = `QRCode${taskName ? " " + taskName : ""}.png`;
+    downloadLink.href = pngFile;
+    downloadLink.click();
+  };
+  // Load SVG data into image element
+  img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+};
