7851beb0c875de173de091cbd474c7d829daf304
phuchoa <phuchoa1202@gmail.com>
2024-05-27 13:03:42 +0700

    Modify the Loading component using Emotion/styled

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { CircularProgress, Box } from '@mui/material';
+import styled from "@emotion/styled";
+import { CircularProgress } from '@mui/material';
 
 interface LoadingProps {
   size?: number;
   thickness?: number;
 }
 
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
 const Loading: React.FC<LoadingProps> = ({ size = 80, thickness = 4 }) => {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+    <Wrapper>
       <CircularProgress size={size} thickness={thickness} />
-    </Box>
+    </Wrapper>
   );
 };
 
